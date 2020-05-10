59b6e6fa59b99d23d618df33371c2561fa375e61
Jordan Gerdin <jordan.gerdin@gmail.com>
2020-05-09 23:09:54 -0600

    small changes to routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,29 @@
 let express = require('express')
-let Sequelize = require('sequelize')
 let db = require('../models')
+let Sequelize = require('sequelize')
 let Student = db.Student
 
 let router = express.Router()
 
 // Returns all students as JSON
 router.get('/students', function(req, res, next) {
-    Student.findAll( {order: ['name']}).then( students => {
+    Student.findAll( { order: ['starID']}).then( students => {
         return res.json(students)
     }).catch( err => next(err))
 })
 
 // Create a new student and return 201 status (ok)
 router.post('/students', function(req, res, next) {
-    Student.create(req.body).then( (data) => {
+    Student.create(req.body).then( data => {
         return res.status(201).send('ok')
-    }).catch( err=> {
+    }).catch( err => {
         if (err instanceof Sequelize.ValidationError) {
             let messages = err.errors.map( e => e.message )
             // 400 = bad request from user
+            console.log(messages)
             return res.status(400).json(messages)
         }
+        console.log(err.data)
         return next(err)
     })
 })
@@ -30,7 +32,11 @@ router.post('/students', function(req, res, next) {
 router.patch('/students/:id', function(req, res, next) {
     Student.update( req.body, { where: { id: req.params.id } })
         .then( rowsModified => {
-            return res.send('ok')
+            if (!rowsModified[0]) {
+                //404 = not found, student with this ID not found
+            } else {
+                return res.send('ok')
+            }
         }).catch( err => {
             if (err instanceof Sequelize.ValidationError) {
                 let messages = err.errors.map( (e) => e.message)
